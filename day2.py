1dbc2ec51b804ac059504936d95c0133a2167fa7
suryanshpandey014-dotcom <suryanshpandey014@gmail.com>
2026-04-18 16:23:27 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,13 @@
+# Even or Odd
+num = int(input("Enter number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Pass or Fail
+marks = int(input("Enter marks: "))
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
